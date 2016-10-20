741eae4f340a8a6559d128e2308dd01d8a0e772a
Wezley Sherman <Wezley@wezleys-air.mobile.asu.edu>
2016-10-20 15:40:37 -0700

    Started networking adapter

--- core/src/com/zombie/game/PCControls.java
+++ core/src/com/zombie/game/PCControls.java
@@ -3,6 +3,7 @@ package com.zombie.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
@@ -28,7 +29,7 @@ public class PCControls extends Controls
 	
 	public void checkInput() 
 	{
-		float rotation = computeRotation(mousePos, player.getPosVector());
+		float rotation = computeRotation();
 		player.setRot(rotation);
 		if(Gdx.input.isKeyPressed(inputKeyForward))
 		{
@@ -48,19 +49,26 @@ public class PCControls extends Controls
 			super.moveRight(player.getMoveSpeed());
 		}
 		
-		if(Gdx.input.isButtonPressed(Keys.SPACE))
+		if(Gdx.input.isKeyPressed(Keys.SPACE))
 		{
 			super.fireWeapon(new Weapon());
 		}
+		Vector3 point = mainCam.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
+
+		if(Gdx.input.isKeyPressed(Keys.L))
+		{
+			System.out.println("Mouse Pos: (" + point.x + ", " + point.y + ") Player: (" + player.getX() + ", " + player.getY()+") + " + rotation);
+		
+		}
 	}
 	
-	private float computeRotation(Vector2 obj, Vector2 tar)
+
+	private float computeRotation()
 	{
 		Vector3 point = mainCam.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
-		Vector3 objPos = mainCam.unproject(new Vector3(tar.x, tar.y, 0));
-		double screenHeight = Gdx.graphics.getHeight();
-
-	    float angle = (float)((Math.atan2(point.y - (screenHeight - objPos.y), point.x - objPos.x) * (180/Math.PI))+90);
+		Vector3 objPos = new Vector3(player.getX(), player.getY(), 0);
+		
+	    float angle = (float)((Math.atan2(point.y - objPos.y, point.x - objPos.x) * (180/Math.PI)));
 	    
 		return angle;
 	}
